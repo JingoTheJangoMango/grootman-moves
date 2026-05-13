56cf6f2bc8b109bff156f23bde282445c9ab85a2
JingoTheJangoMango <168852143+JingoTheJangoMango@users.noreply.github.com>
2026-05-13 12:10:43 +0200

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,54 @@
+import tidalapi
+import time
+def main():
+
+    session = tidalapi.Session()
+    session.login_oauth_simple()
+
+    playlist_lis = playlist_collector(session)
+
+    for playlist_id in playlist_lis:
+        pass
+
+
+
+
+def playlist_collector(session):
+
+    playlist_lis = []
+
+    for x in session.user.playlists():
+        playlist_lis.append(x.id)
+
+    return playlist_lis
+
+def track_collector(session, playlist_id):
+
+    track_count = session.playlist(playlist_id).get_tracks_count()
+    track_id_lis = []
+
+    if track_count > 400:
+
+        for track_id in session.playlist(playlist_id):
+            track_id_lis.append(track_id)
+
+            if len(track_id):
+                pass
+
+
+start = time.perf_counter()
+result = []
+
+result = [x for x in range(500_000_000)]
+result = result[:len(result)//2]
+
+end = time.perf_counter()
+
+print(end-start)
+
+
+"""for x in range(500_000_000):
+    result.append(x)
+
+    if len(result) == 250_000_000:
+        break"""
