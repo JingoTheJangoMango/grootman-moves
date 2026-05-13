b480ed6ce2d52e9a5fb92c88059ce7da98ac0e8f
Jingo <jingobejango@gmail.com>
2026-05-13 17:07:38 +0200

    Added some functions

--- lab2.py
+++ lab2.py
@@ -1,5 +1,8 @@
 import tidalapi
-import time
+
+
+
+
 def main():
 
     session = tidalapi.Session()
@@ -8,8 +11,11 @@ def main():
     playlist_lis = playlist_collector(session)
 
     for playlist_id in playlist_lis:
-        pass
+        
+        track_id_lis = track_collector(session, playlist_id)
+        playlist_name = playlist_namer(session)
 
+        playlist_creator(session, playlist_name, track_id_lis)
 
 
 
@@ -22,18 +28,59 @@ def playlist_collector(session):
 
     return playlist_lis
 
+
+
 def track_collector(session, playlist_id):
 
     track_count = session.playlist(playlist_id).get_tracks_count()
     track_id_lis = []
+    answer = input(f"The track count is {track_count}, split the playlist:(Y/N)")
 
-    if track_count > 400:
+    if answer.lower() == "y":
 
         for track_id in session.playlist(playlist_id):
-            track_id_lis.append(track_id)
+            track_id_lis.append(track_id.id)
 
-            if len(track_id):
-                pass
+            if len(track_id) == track_count // 2:
+                break
+    else:
+        print('Playlist Skipped')
+
+    return track_id_lis
 
 
 
+def  playlist_namer(session):
+
+    playlist_lis = session.user.favorites.playlists(order="DATE", limit=10, order_direction="DESC")
+    playlist_name_lis = []
+
+    for playlist in playlist_lis:
+        if playlist.name.is_digit():
+            playlist_name_lis.append(playlist.name)
+
+    playlist_name_lis.sort(reverse=True)
+
+    return playlist_name_lis[0]
+
+
+
+def playlist_creator(session, playlist_name, track_id_lis):
+
+    playlist = session.user.LoggedInUser.create_playlist(f"{playlist_name}", "")
+    session.playlist.UserPlaylist(playlist.id).add(track_id_lis)
+
+    print('Playlist Created')
+
+
+
+def track_remover(session, playlist_id_lis, track_id_lis):
+    pass
+
+
+
+
+
+
+
+main()
