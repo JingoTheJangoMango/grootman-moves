30a676600ab9d7ea600aed50e6d0a65be90d29cd
Jingo <jingobejango@gmail.com>
2026-05-14 15:20:53 +0200

    Did some shyte

--- lab2.py
+++ lab2.py
@@ -1,6 +1,15 @@
 import tidalapi
+from tidalapi.types import PlaylistOrder
+from tidalapi.types import OrderDirection
 
+"""Potentional problems:
+    1. Split tracks: Since the track collector splits the tracks based on a fixed number
+        certain tracks may appear in a seperate playlist despite being in the same album. Please fix.
 
+    2. The server side itself may pose problems, specifically regarding requests, Tidal is as tight as- 
+        a nun when it comes to request rates.
+
+"""
 
 
 def main():
@@ -9,54 +18,30 @@ def main():
     session.login_oauth_simple()
 
     playlist_lis = playlist_collector(session)
+    playlist_name = playlist_namer(session)
+    playlist_name = int(playlist_name)
 
     for playlist_id in playlist_lis:
-        
+
         track_id_lis = track_collector(session, playlist_id)
-        playlist_name = playlist_namer(session)
 
-        playlist_creator(session, playlist_name, track_id_lis)
+        if len(track_id_lis):
 
+            playlist_name += 1
 
+            playlist_creator(session, playlist_name, track_id_lis)
+            print("Playlist Created")
 
-def playlist_collector(session):
-
-    playlist_lis = []
-
-    for x in session.user.playlists():
-        playlist_lis.append(x.id)
-
-    return playlist_lis
-
-
-
-def track_collector(session, playlist_id):
-
-    track_count = session.playlist(playlist_id).get_tracks_count()
-    track_id_lis = []
-    answer = input(f"The track count is {track_count}, split the playlist:(Y/N)")
-
-    if answer.lower() == "y":
-
-        for track_id in session.playlist(playlist_id):
-            track_id_lis.append(track_id.id)
-
-            if len(track_id) == track_count // 2:
-                break
-    else:
-        print('Playlist Skipped')
-
-    return track_id_lis
 
 
 
 def  playlist_namer(session):
 
-    playlist_lis = session.user.favorites.playlists(order="DATE", limit=10, order_direction="DESC")
+    playlist_lis = session.user.favorites.playlists(order=PlaylistOrder.DateCreated, limit=10, order_direction=OrderDirection.Descending)
     playlist_name_lis = []
 
     for playlist in playlist_lis:
-        if playlist.name.is_digit():
+        if playlist.name.isdigit():
             playlist_name_lis.append(playlist.name)
 
     playlist_name_lis.sort(reverse=True)
@@ -65,22 +50,49 @@ def  playlist_namer(session):
 
 
 
+def playlist_collector(session):
+    #Collects all playlists in user's collection and adds them to a list that is returned
+
+    playlist_lis = []
+
+    for playlist_obj in session.user.favorites.playlists(limit=10):
+        playlist_lis.append(playlist_obj.id)
+
+    return playlist_lis
+
+
+
+def track_collector(session, playlist_id):
+    #Uses playlist ids to get track count of respective playlists and then splits tracks in two based on prompt
+
+    track_count = session.playlist(playlist_id).get_tracks_count()
+    track_id_lis = []
+    answer = input(f"The track count is {track_count}, split the playlist:(Y/N)")
+
+    if answer.lower() == "y":
+
+        for track_obj in session.playlist(playlist_id).tracks():
+            track_id_lis.append(track_obj.id)
+
+            if len(track_id_lis) == track_count // 2:
+                break
+    else:
+        print('Playlist Skipped')
+
+    return track_id_lis
+
+
+
 def playlist_creator(session, playlist_name, track_id_lis):
 
-    playlist = session.user.LoggedInUser.create_playlist(f"{playlist_name}", "")
-    session.playlist.UserPlaylist(playlist.id).add(track_id_lis)
+    string_name = str(playlist_name)
 
-    print('Playlist Created')
+    playlist = session.user.create_playlist(f"{string_name.zfill(4)}", "")
+    playlist.add(track_id_lis)
 
 
 
-def track_remover(session, playlist_id_lis, track_id_lis):
+
+def track_remover(session, playlist_id, track_id_lis):
     pass
 
-
-
-
-
-
-
-main()
