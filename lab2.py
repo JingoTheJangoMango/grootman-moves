1c2b47425e6244253fc877c0d767b0bd57393f55
Jingo <jingobejango@gmail.com>
2026-05-15 19:20:39 +0200

    Fixed some shyte

--- lab2.py
+++ lab2.py
@@ -22,21 +22,34 @@ def main():
 
     session = tidalapi.Session()
     session.login_oauth_simple()
+    counter = 0
+    #playlist_name = playlist_namer(session)
 
-    playlist_lis = playlist_collector(session)
-    playlist_name = playlist_namer(session)
-    playlist_name = int(playlist_name)
+    total_playlist_count = session.user.favorites.get_playlists_count()
 
-    for playlist_id in playlist_lis:
+    while counter < total_playlist_count:
 
-        track_id_lis = track_collector(session, playlist_id)
+        playlist_obj_lis = playlist_collector(session)
+        
+        #playlist_name = int(playlist_name)
 
-        if len(track_id_lis):
+        for playlist_obj in playlist_obj_lis:
 
-            playlist_name += 1
+            if playlist_obj.name.isdigit() or playlist_obj.name.endswith(".5"):
 
-            playlist_creator(session, playlist_name, track_id_lis)
-            print("Playlist Created")
+                track_id_lis = track_collector(session, playlist_obj)
+
+                if len(track_id_lis):
+
+                    playlist_creator(session, playlist_obj, track_id_lis)
+                    track_remover(session, playlist_obj, track_id_lis)
+                    playlist_renamer(session, playlist_obj)
+                else:
+                    playlist_renamer(session, playlist_obj)
+        
+        print("Batch Completed")
+        counter += 50
+        
 
 
 
@@ -59,25 +72,30 @@ def  playlist_namer(session):
 def playlist_collector(session):
     #Collects all playlists in user's collection and adds them to a list that is returned
 
-    playlist_lis = []
+    playlist_obj_lis = []
 
-    for playlist_obj in session.user.favorites.playlists(limit=10):
-        playlist_lis.append(playlist_obj.id)
+    for playlist_obj in session.user.favorites.playlists(limit=50, order=PlaylistOrder.Name, order_direction=OrderDirection.Descending):
 
-    return playlist_lis
+        if playlist_obj.name.isdigit():
+            playlist_obj_lis.append(playlist_obj)
+        elif playlist_obj.name.endswith(".5"):
+            playlist_obj_lis.append(playlist_obj)
+
+    return playlist_obj_lis
 
 
 
-def track_collector(session, playlist_id):
+def track_collector(session, playlist_obj):
     #Uses playlist ids to get track count of respective playlists and then splits tracks in two based on prompt
 
-    track_count = session.playlist(playlist_id).get_tracks_count()
+    track_count = session.playlist(playlist_obj.id).get_tracks_count()
     track_id_lis = []
-    answer = input(f"The track count is {track_count}, split the playlist:(Y/N)")
+    #answer = input(f"The track count is {track_count}, split the playlist:(Y/N)")
+    limit = 300
 
-    if answer.lower() == "y":
+    if track_count >= limit:
 
-        for track_obj in session.playlist(playlist_id).tracks():
+        for track_obj in session.playlist(playlist_obj.id).tracks():
             track_id_lis.append(track_obj.id)
 
             if len(track_id_lis) == track_count // 2:
@@ -89,16 +107,34 @@ def track_collector(session, playlist_id):
 
 
 
-def playlist_creator(session, playlist_name, track_id_lis):
+def playlist_creator(session, playlist_obj, track_id_lis):
 
-    string_name = str(playlist_name)
-
-    playlist = session.user.create_playlist(f"{string_name.zfill(4)}", "")
+    playlist = session.user.create_playlist(f"{playlist_obj.name.zfill(4)}.5", "")
     playlist.add(track_id_lis)
+    
+    print("Playlist Created")
 
 
 
+def track_remover(session, playlist_obj, track_id_lis):
 
-def track_remover(session, playlist_id, track_id_lis):
-    pass
+    new_track_id_lis = [str(track) for track in track_id_lis]
+    session.playlist(playlist_obj.id).delete_by_id(new_track_id_lis)
 
+    print("All Tracks deleted")
+
+
+
+def playlist_renamer(session, playlist_obj):
+  
+
+    if playlist_obj.name.endswith(".5"):
+       
+        session.playlist(playlist_obj.id).edit(f"{playlist_obj.name.zfill(6)}")
+        print(f"Playlist {playlist_obj.name} renamed")
+    else:
+        session.playlist(playlist_obj.id).edit(f"{playlist_obj.name.zfill(4)}")
+        print(f"Playlist {playlist_obj.name} Renamed")
+
+
+main()
